bf86ae90e1ca00b7cc15d9a01d41618b613c739f
Mekha1104 <gtrcooie@gmail.com>
2023-08-16 22:21:43 +0700

    05-8

--- Homework/HW05/hw05-8-max-value.cpp
+++ Homework/HW05/hw05-8-max-value.cpp
@@ -23,20 +23,19 @@
 #include <stdio.h>
 
 int main() {
-    int num1, num2, num3;
+    int Num1, Num2, Num3;
+    scanf("%d %d %d", &Num1, &Num2, &Num3);
+    printf("1st Number = %d\n", Num1);
+    printf("2nd Number = %d\n", Num2);
+    printf("3rd Number = %d\n", Num3);
 
-    printf("Enter three numbers:\n");
-    scanf("%d %d %d", &num1, &num2, &num3);
-
-    printf("1st Number = %d\n", num1);
-    printf("2nd Number = %d\n", num2);
-    printf("3rd Number = %d\n", num3);
-
-    if (num1 >= num2 && num1 >= num3) {
-        printf("The 1st Number is the greatest among three\n");
-    } else if (num2 >= num1 && num2 >= num3) {
+    if (Num1 > Num2 && Num1 > Num3) {
+        printf("The 1rd Number is the greatest among three\n");
+    }
+    else if (Num2 > Num1 && Num2 > Num3) {
         printf("The 2nd Number is the greatest among three\n");
-    } else {
+    }
+    else {
         printf("The 3rd Number is the greatest among three\n");
     }
 
